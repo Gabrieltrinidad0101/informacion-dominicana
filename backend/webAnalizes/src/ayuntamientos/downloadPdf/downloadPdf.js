1e485dc5d38c7da48a4a2bd25de85b3c4bbe058e
gabriel <gabrielqwes123@gmail.com>
2024-02-10 17:09:01 -0400

    feat: analize text with performance

--- backend/webAnalizes/src/ayuntamientos/downloadPdf/downloadPdf.js
+++ backend/webAnalizes/src/ayuntamientos/downloadPdf/downloadPdf.js
@@ -4,33 +4,65 @@ const fs = require("fs")
 const path = require("path")
 
 const townHalls = [{
-    link:"https:ayuntamientojarabacoa.gob.do",
+    link: "https:ayuntamientojarabacoa.gob.do",
     name: "ayuntamientojarabacoa"
 }]
 
 
-const downloadPdf = async ()=>{
-    const browser = await puppeteer.launch()
+const downloadPdf = async () => {
+    const browser = await puppeteer.launch({
+        headless: false
+    })
     const page = await browser.newPage()
-    
-    for(const {link,name} of townHalls ){
-        await page.goto(`${link}/transparencia/documentos/nomina/`,{ waitUntil: 'load' })
+
+    for (const { link, name } of townHalls) {
+        const urlLink = `${link}/transparencia/documentos/nomina/`
+        await page.goto(urlLink, { waitUntil: 'load' })
         const nominationsByYear = await page.$$(".el-folder.col-lg-6.col-md-6.col-sm-6 > a")
-        for(const nomination of nominationsByYear.reverse()){
-            await page.goto(await page.evaluate(el => el.href, nomination),{ waitUntil: 'load' })
+        console.log(`Downloading pdf from ${urlLink}`)
+        for (const nomination of nominationsByYear.reverse()) {
+            await page.goto(await page.evaluate(el => el.href, nomination), { waitUntil: 'load' })
             const downloadsLink = await page.$$(".btn.btn-descargar.pull-right")
-            for(const downloadLink of downloadsLink){
+            for (const downloadLink of downloadsLink) {
                 const link = await page.evaluate(el => el.href, downloadLink)
-                await savePdf(name,link)      
+                console.log(`   pdf: ${link}`)
+                await savePdf(name, link)
             }
         }
     }
     browser.close()
 }
 
-const savePdf = (name,link)=> new Promise((res,rej)=>{
-    const folderName = path.join(__dirname,`pdf/${name}`)
-    fs.mkdir(folderName,_=>{})
+const getFileNameFromURL = url => {
+    const parsedUrl = new URL(url);
+    const pathname = parsedUrl.pathname;
+    const filename = path.basename(pathname);
+    return filename;
+}
+
+function fileExists(filePath) {
+    return new Promise((resolve, reject) => {
+        fs.access(filePath, fs.constants.F_OK, (err) => {
+            if (err) {
+                if (err.code === 'ENOENT') {
+                    resolve(false); // File does not exist
+                } else {
+                    reject(err); // Other error
+                }
+            } else {
+                resolve(true); // File exists
+            }
+        });
+    });
+}
+
+const savePdf = (name, link) => new Promise((res, rej) => {
+    const fileName = getFileNameFromURL(link)
+    const folderPath = path.join(__dirname, `pdf/${name}`)
+    const exist = fileExists(path.join(folderPath, fileName))
+    if (exist) return
+    const folderName = folderPath
+    fs.mkdir(folderName, _ => { })
     const dl = new DownloaderHelper(link, folderName);
     dl.on('end', res);
     dl.on('error', rej);
@@ -38,4 +70,4 @@ const savePdf = (name,link)=> new Promise((res,rej)=>{
 })
 
 
-module.exports = {downloadPdf}
+module.exports = { downloadPdf }
--- data.js
+++ data.js
@@ -1,12 +1,11 @@
-const data = `
-842 LIBRADO SANTOS 050-0011361-2 3639.00 200010510258506 VIGILANTE NOCTURNO DEL PARQUE DURRT
+const dataText = `842 LIBRADO SANTOS 050-0011361-2 3639.00 200010510258506 VIGILANTE NOCTURNO DEL PARQUE DURRT
 4702 LIC. BELGICA ELCIRA CASTELLANOS PUJOLS0000-00-00 001-1762816-4 35,000.00 ENC. DIVISION DE SERVICIOS GENERALE
 382 LIC. JOSE ENRIQUE TAVAREZ S 050-0028516-2 43,525.00 200010510415103 REGIDOR A
 1622 LIC. RAFAEL AGUSTIN ABREU MENDEZ 05000395573 30,000.00 200010510310680 DIRECTOR DE RECURSOS HUMANOS A
 730 LICDA. MARICELA ALT. GENAO DE JIMENEZ 050-0006288-4 43,525.00 200010510417266 REGIDORA A
 873 LICDA. MARY AYBAR RAMOS 0000-00-00 050-0035361-4 20,000.00 200010510245111 CONTRALORA MUNICIPAL A
 66 LICDA RHINA CHONG SURIEL 00000000 05000285857 30,000.00 200010510414120 ENC.DPATO. ADM. FINANCIERO A
-kal LICDA. YANINA GALAN DE LEON 050-0040944-0 30,000.00 200010510417279 TESORERA MUNICIPAL A
+123 LICDA. YANINA GALAN DE LEON 050-0040944-0 30,000.00 200010510417279 TESORERA MUNICIPAL A
 534 LIDIA ALT. CELESTINO GARCIA 0000-00-00 050-0009274-1 3174.00 200010510247151 CONSERJE MERCADO A
 32 LIDIO ANT. NUNEZ POLO 0000-00-00 050-0022753-7 5,871.00 CAPATAZ A
 8468 LIGA MARCOS PENA O DAVID DURAN 00000000 2,500.00 0 AYUDA INSTITUCION A
@@ -25,6 +24,67 @@ kal LICDA. YANINA GALAN DE LEON 050-0040944-0 30,000.00 200010510417279 TESORERA
 47% LUZ BERENICE VICIOSO ACOSTA 0000-00-00 050-0036101-3 8,000.00 200010510414735 SECRETARIA A
 4775 LUZ LORELY BAUTISTA HERRERA 0000-00-00 050-0042819-2 5,000.00 ASISTENTE DEL ALCALDE EN HATO VIEXD
 `
+//All the text is analize using only one for has better perfomarce
+const numbers = new Set(["0","1","2","3","4","5","6","7","8","9"])
+const letters = new Set(["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","ñ","z","x","c","v","b","n","m","Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Ñ","Z","X","C","V","B","N","M"])
 
+//by each line search number after find a number we continue since find to space after that 
+//all the continue text is the name of emploment since we find a 0 because the rnc start with 0 in all the case
+const returnGetName = ()=>{
+    let findNumber = false
+    let startGetName = false
+    let name = ""
+    let stop = false
+    return (character)=>{
+        if(stop) return name
+        if(!findNumber) findNumber = numbers.has(character)
+        if(numbers.has(character) && name != ""){
+            stop = true
+            return name
+        }
+        if(startGetName) name += character
+        if(findNumber && character == " ") startGetName = true
+    }
+}
+
+const returnGetPosition = ()=>{
+    let position = ""
+    let start = false
+    let findAletter = false
+    let text = ""
+    return (character)=>{
+        if(!start && text !== "") text += character
+        if(character === ".") text = character
+        if(text === ".00 ") start = true 
+        if(text.length == 4) text = ""
+        if(start && !findAletter) {
+            findAletter = letters.has(character)
+        }
+        if(findAletter) position += character
+        return position
+    }
+}
+
+
+const getNameAndPosition = ()=>{
+    let name = ""
+    let position = ""
+    let getName  = returnGetName()
+    let getPosition  = returnGetPosition()
+    let data = []
+    for(let i = 0; i < dataText.length; i++){
+        if(dataText[i] === "\n" || dataText[i] === "\r" || i == dataText.length - 1) {
+            getName  = returnGetName()
+            getPosition  = returnGetPosition()
+            console.log({name,position})
+            if(name != "" && position != "") data.push({name,position})
+        }
+        name = getName(dataText[i])
+        position = getPosition(dataText[i])
+    }
+    console.log(data.length)
+}
+
+getNameAndPosition()
 
 // (?:\d{1,3},)?\d{1,4}\.\d{2}
