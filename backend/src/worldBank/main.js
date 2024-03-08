1c7570b7a6187f9e37d55b6ceab0a28ddd1f3313
gabriel <gabrielqwes123@gmail.com>
2024-03-08 11:29:14 -0400

    feat: data

--- backend/src/worldBank/main.js
+++ backend/src/worldBank/main.js
@@ -2,12 +2,13 @@ const jsonData = require("./readXmlFile")
 const analizeJson = require("./analizeJson")
 const fs = require('fs')
 const { constants } = require("../constants")
+const path = require("path")
 
 const concepts = analizeJson(jsonData)
 for (const concept of Object.keys(concepts)){
-    fs.mkdir(`../processedData/worldBank/${concept}`,{},_=> {})
+    fs.mkdir(`../../../datas/worldBank/${concept}`,{},_=> {})
     concepts[concept].forEach((value,description)=>{
-        const fileDirrection = path.JSON(constants.dataWorldBank(concept),`/${description}.json`)
+        const fileDirrection = path.join(constants.dataWorldBank(concept),`/${description}.json`)
         fs.writeFile(fileDirrection,JSON.stringify(value),_=> {})
     })
 }
