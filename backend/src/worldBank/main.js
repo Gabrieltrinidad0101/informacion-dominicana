08edb5490a302754513b7b460f145bc242dfa65c
gabriel-trinidad-ethicscode <gtrinidad@ethicscode.ai>
2025-03-08 15:19:55 -0400

    feat: window support

--- backend/src/worldBank/main.js
+++ backend/src/worldBank/main.js
@@ -9,7 +9,10 @@ const concepts = analyzeJson(jsonData)
 for (const concept of Object.keys(concepts)){
     fs.mkdir(`../../../datas/worldBank/${concept}`,{},_=> {})
     concepts[concept].forEach((value,description)=>{
-        const fileDirrection = path.join(constants.dataWorldBank(concept),`/${description.replaceAll(":","-")}.json`)
+        const fileDirrection = path.join(constants.dataWorldBank(concept),`/${description
+            .replaceAll(":","-")
+            .replaceAll(">","mayor")
+        }.json`)
         fs.writeFile(fileDirrection,JSON.stringify(value),_=> {})
     })
 }
