b8cf1d3aed23a2da02884bc0cfc037645a0e19a3
gabriel-trinidad-ethicscode <gtrinidad@ethicscode.ai>
2025-08-10 16:21:26 -0400

    docker refactor

--- projects/download/main.js
+++ projects/download/main.js
@@ -1,6 +1,6 @@
+import { Download } from "./download.js"
 import { EventBus } from "../eventBus/eventBus.js"
 import { FileManager } from "../filesAccess/fileAccess.js"
-import { Download } from "./download.js"
 
 const eventBus = new EventBus()
 
