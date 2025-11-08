5a27db8cd9109bcb53edbac36927e907eda6c7b5
Gabriel Trinidad <gabrielqwes123@gmail.com>
2025-11-08 14:10:34 -0400

    test

--- projects/downloadLinks/main.js
+++ projects/downloadLinks/main.js
@@ -2,4 +2,4 @@ import { DownloadTownHallData } from "./websites/townHall/townHall.js"
 import { EventBus } from "../eventBus/eventBus.js"
 
 const eventBus = new EventBus()
-new DownloadTownHallData(eventBus)
+new DownloadTownHallData(eventBus)     
