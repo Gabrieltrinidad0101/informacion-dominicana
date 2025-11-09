ae32e89e3e5c56d83b57dcb1fc4211ddf6657a96
Gabriel Trinidad <gabrielqwes123@gmail.com>
2025-11-09 14:44:04 -0400

    fix: auth

--- projects/admin/src/theme/Layout/index.js
+++ projects/admin/src/theme/Layout/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Layout from '@theme-original/Layout';
-import { ClerkProvider,useClerk } from '@clerk/clerk-react'
+import { ClerkProvider, useClerk, useUser } from '@clerk/clerk-react'
+import Login from '../../views/Login/Login';
+
+
 
 const PUBLISHABLE_KEY = process.env.REACT_APP_PUBLISHABLE_KEY
 
@@ -8,10 +11,18 @@ if (!PUBLISHABLE_KEY) {
   throw new Error('Add your Clerk Publishable Key to the .env file')
 }
 
+const AuthLayout = (props) => {
+  const { isSignedIn } = useUser();
+  if (!isSignedIn) {
+    return <Login></Login>
+  }
+  return  <Layout {...props} />
+}
+
 export default function LayoutWrapper(props) {
   return (
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-        <Layout {...props} />
-      </ClerkProvider>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <AuthLayout {...props} />
+    </ClerkProvider>
   );
 }
--- projects/admin/src/views/Eventos/Eventos.jsx
+++ projects/admin/src/views/Eventos/Eventos.jsx
@@ -1,15 +1,8 @@
 import { useLocation } from "react-router";
 import { Evento } from "../../components/evento/Evento";
 import EventosCss from "./Eventos.module.css";
-import { useUser } from "@clerk/clerk-react";
-import BrowserOnly from '@docusaurus/BrowserOnly';
 
 export function Eventos({exchangeName}) {
-  const {isSignedIn} = useUser();
-  if(!isSignedIn) {
-    window.location.href = `/login`
-    return <div>Loading...</div>
-  }
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const exchangeNames = ["downloadLinks","downloads","postDownloads","extractedTexts","analyzeExtractedTexts","aiTextAnalyzers","insertDatas","payrollExportToJsons","payrolls"]
@@ -23,9 +16,7 @@ export function Eventos({exchangeName}) {
   
   return (
     <div className={EventosCss.eventos} >
-      <BrowserOnly>
         <Evento exchangeName={exchangeName} queryParams={queryParams} />
-      </BrowserOnly>
     </div>
   );
 }
