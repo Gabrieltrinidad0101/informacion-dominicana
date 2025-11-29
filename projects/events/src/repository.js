82e78efaaf94d5aa7ad07d54e74b2ca01e7d5e7b
Gabriel Trinidad <gabrielqwes123@gmail.com>
2025-11-29 14:12:55 -0400

    fix: value

--- projects/events/src/repository.js
+++ projects/events/src/repository.js
@@ -32,6 +32,7 @@ export class EventsRepository {
         try {
             const data = JSON.parse(await fs.readFile('./projects/events/src/defaultEvents.json'))
             for (const [key, value] of Object.entries(data.downloadLinks)) {
+                console.log(value)
                 const Model = mongoose.model(key, dynamicSchema);
                 Model.updateOne(
                     { _id: new mongoose.Types.ObjectId(value._id) },
