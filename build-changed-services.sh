94f9973ad743192e38fad11707da648c0a7e8032
Gabriel Trinidad <gabrielqwes123@gmail.com>
2025-12-14 19:54:00 -0400

    fix

--- build-changed-services.sh
+++ build-changed-services.sh
@@ -10,14 +10,15 @@ echo "$CHANGED"
 for SERVICE in "${!SERVICES[@]}"; do
   PATH_TO_WATCH=${SERVICES[$SERVICE]}
   KEY_DEPLOY=${DEPLOY[$SERVICE]}
+  WEBHOOK="${!KEY_DEPLOY}"
 
   if echo "$CHANGED" | grep -q "^$PATH_TO_WATCH/"; then
     echo "🔄 Changes detected in $PATH_TO_WATCH → Rebuilding $SERVICE..."
-    echo $PACKAGE_TOKEN | docker login -u gabrielopensource --password-stdin ghcr.io
-    docker compose -f docker-compose-pro.yml build $SERVICE
-    docker tag informacion-dominicana-$SERVICE:latest ghcr.io/gabrieltrinidad0101/informacion-dominicana-$SERVICE:latest
-    docker push ghcr.io/gabrieltrinidad0101/informacion-dominicana-$SERVICE:latest
-    curl -X POST $KEY_DEPLOY
+    # echo $PACKAGE_TOKEN | docker login -u gabrielopensource --password-stdin ghcr.io
+    # docker compose -f docker-compose-pro.yml build $SERVICE
+    # docker tag informacion-dominicana-$SERVICE:latest ghcr.io/gabrieltrinidad0101/informacion-dominicana-$SERVICE:latest
+    # docker push ghcr.io/gabrieltrinidad0101/informacion-dominicana-$SERVICE:latest
+    curl -X POST $WEBHOOK
     continue
   fi
 
