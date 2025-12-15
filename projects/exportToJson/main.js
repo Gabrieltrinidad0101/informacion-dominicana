18683d13019d30140e7f8d57550102d49abf64bc
Gabriel Trinidad <gabrielqwes123@gmail.com>
2025-12-14 20:18:40 -0400

    fix

--- projects/exportToJson/main.js
+++ projects/exportToJson/main.js
@@ -6,3 +6,4 @@ import { FileManagerClient } from "../fileManagerClient/main.js"
 const fileAccess = new FileManagerClient();
 const repository = new Repository();
 new Payroll(repository,eventBus,fileAccess);
+ 
