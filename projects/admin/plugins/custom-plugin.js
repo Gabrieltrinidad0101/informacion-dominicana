aa0e6eb45dc41ff31e210753586af9b69f9c810c
Gabriel Trinidad <gabrielqwes123@gmail.com>
2025-12-17 22:43:46 -0400

    fix:

--- projects/admin/plugins/custom-plugin.js
+++ projects/admin/plugins/custom-plugin.js
@@ -13,7 +13,7 @@ export default function customWebpackPlugin() {
           }),
           new webpack.DefinePlugin({
             'process.env.REACT_APP_SERVER_URL': JSON.stringify(process.env.REACT_APP_SERVER_URL || 'http://localhost:5500/data'),
-            'process.env.REACT_APP_API_URL': JSON.stringify(process.env.REACT_APP_API_URL || 'http://informacion-dominicana-events-j3tbhk-08ef2c-185-188-249-178.traefik.me/events'),
+            'process.env.REACT_APP_API_URL': JSON.stringify(process.env.REACT_APP_API_URL || 'http://127.0.0.1:3001/events'),
             'process.env.REACT_APP_PUBLISHABLE_KEY': JSON.stringify(process.env.REACT_APP_PUBLISHABLE_KEY),
         }),
         ],
